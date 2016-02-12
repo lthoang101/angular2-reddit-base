21fedb456b53d8cdf50e766096d82c91a9be6151
Loc <lthoang@live.com>
2016-02-11 22:07:56 -0800

    Replace HelloWorld component with new RedditApp component
    Created a new RedditApp component and edited the html file to load the
    new component. Also added some menu items to index.html. Using Semantic
    UI for styling.

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@ import { bootstrap } from "angular2/platform/browser";
 import { Component } from "angular2/core";
 import { NgFor } from "angular2/common";
 
+/*
 @Component({
     selector: 'hello-world',
     template: `
@@ -17,5 +18,39 @@ class HelloWorld {
         this.names = ['Ari', 'Carlos', 'Felipe', 'Nate'];
     }
 }
+*/
 
-bootstrap(HelloWorld);
+@Component({
+    selector: 'reddit',
+    template: `
+        <form class="ui large form segment">
+            <h3 class="ui header">Add a Link</h3>
+
+            <div class="field">
+                <label for="title">Title:</label>
+                <input name="title" #newtitle>
+            </div>
+
+            <div class="field">
+                <label for="link">Link:</label>
+                <input name="link" #newlink>
+            </div>
+
+            <button (click)="addArticle(newtitle, newlink)"
+                    class="ui positive right floated button">
+                    Submit link
+            </button>
+        </form>
+  `
+})
+class RedditApp {
+
+    constructor() {
+    }
+
+    addArticle(title: HTMLInputElement, link: HTMLInputElement): void {
+        console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    }
+}
+
+bootstrap(RedditApp);
